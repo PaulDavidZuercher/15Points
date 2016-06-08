babebab0fb1862512cb04618d45dbdd9761e81e9
xX420Xx <oneplus.push1@gmail.com>
2016-06-08 15:44:14 +0200

    loginProc
    lol

---
+++ loginProc.php
@@ -0,0 +1,14 @@
+<html>
+<body>
+
+<form action="loginProc.php">
+Username:<br>
+<input type="text" name="uid">
+<br>
+Password:<br>
+<input type="password" name="psw">
+</form>
+
+
+</body>
+</html>
