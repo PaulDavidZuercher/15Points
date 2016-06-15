5ba7f2ec4304866fa0ef6af0a8c1a02e83afe823
THEPADA <PaulDavidZuercher@users.noreply.github.com>
2016-06-15 12:50:40 +0200

    Finished login.php

--- login.php
+++ login.php
@@ -1,25 +1,35 @@
-<!DOCTYPE html>
 <html>
-  
-  	<head>
-		  <title>Login</title>
-		  <link rel="stylesheet" type="text/css" href="formate.css">
-		  <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
-  	</head>
-
+  <head>
+    <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
+    <script type = "text/javascript">
+      function checkUserPass()
+      {
+        $.ajax({
+          type: "POST",
+          url: "/loginProc.php",
+          data: {userName: $("#userName").val, passWord: $('passWord').val},
+          success:function(result){('#output').val(result)}
+      });
+        
+      }
+    </script>
+      
+    </script>
+  </head>
   <body>
-		<div class="center">
-		<h1>Einlogen</h1>
-   		<form action="loginProc.php">
-      		Username:<br>
-      		<input type="text" name="uid">
-      		<br>
-      		Password:<br>
-      		<input type="password" name="psw">
-		</form>
-		<a href="register.php">Noch nicht registriert?</a>
-		</div>
- </body>
- 
-</html>
+    <form>
+      Username:<br>
+      <input type="text" name="userName"  id="userName">
+      <br>
+      Password:<br>
+      <input type="password" name="passWord" id="passWord">
+      
+      <div id="output"style="width:100px;height:20px">
+      </div>
+      
+      <button href="#" onclick="checkUserPass();return false;">Login</button>
+    </form>
 
+
+</body>
+</html>
