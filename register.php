a45cf93f005f401e90fd1ea636eb98dd66b0446b
xX420Xx <oneplus.push1@gmail.com>
2016-06-15 12:47:32 +0200

    Create register.php Alpha

---
+++ register.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Sign in</title>
+		<link rel="stylesheet" type="text/css" href="formate.css">
+		<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
+	</head>
+
+	<body>
+		<h1>Registrieren</h1>
+
+		<div class="center">
+
+
+		</div>
+	</body>
+</html>
