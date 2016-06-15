52d0f6477eecb4c764d322a33982ebb5fa444be3
THEPADA <PaulDavidZuercher@users.noreply.github.com>
2016-06-15 11:39:05 +0200

    Formatted File

--- login.php
+++ login.php
@@ -1,13 +1,15 @@
 <html>
-<body>
-
-<form action="loginProc.php">
-Username:<br>
-<input type="text" name="uid">
-<br>
-Password:<br>
-<input type="password" name="psw">
-</form>
+  <head>
+    <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
+  </head>
+  <body>
+    <form action="loginProc.php">
+      Username:<br>
+      <input type="text" name="uid">
+      <br>
+      Password:<br>
+      <input type="password" name="psw">
+    </form>
 
 
 </body>
