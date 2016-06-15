060a6791fc559a3b90eb829f2d55b852883ae50e
THEPADA <PaulDavidZuercher@users.noreply.github.com>
2016-06-15 12:04:53 +0200

    Create loginProc.php

---
+++ loginProc.php
@@ -0,0 +1,20 @@
+<?php
+  session_start();
+  include 'sqlConnect.php'
+  
+  if(!empty($_POST)
+  {
+    $userName = $_POST["userName"];
+    $mysqli->prepare("Select * from user where username = ? && passWord = ?");
+    $mysqli->bind_param("ss", $_POST["userName"], $_POST["passWord"] );
+    if($res = $conn->execute())
+    {
+      if($res->num_rows > 0)
+      {
+        //user exists 
+        $_SESSION["login"] = true;
+        $_SESSION["userName"] = $_POST["userName"]; //todo find more safe version :)
+      }
+    }
+  }
+?>
