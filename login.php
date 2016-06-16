129d634cc44b2e4f34c31d30cd876d9c3a121b40
THEPADA <PaulDavidZuercher@users.noreply.github.com>
2016-06-16 10:48:21 +0200

    Error fixing

--- login.php
+++ login.php
@@ -1,25 +1,25 @@
 <html>
   <head>
     <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
-    <script type = "text/javascript">
-  </head>
-  <body>
-      function checkUserPass()
-      {
+    <script type="text/javascript">
+      function checkUserPass(){
         $.ajax({
           type: "POST",
           url: "/loginProc.php",
-          data: {userName: $("#userName").val, passWord: $('passWord').val},
-          success:function(result){('#output').val(result)}
+          data: {userName: $("#userName").val, passWord: $('#passWord').val},
+          success:function(result){$('#output').val(result)}
       });
       }
     </script>
-    <div>
+  </head>
+  <body>
+    <div id="pwDiv">
       <form>
         Username:<br>
         <input type="text" name="userName"  id="userName">
         <br>
-        Password:<br>
+        Password:
+        <br>
         <input type="password" name="passWord" id="passWord">
         <button href="#" onclick="checkUserPass();return false;">Login</button>
         <div id="output"style="width:100px;height:20px">
