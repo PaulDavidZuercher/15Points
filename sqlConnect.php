3c0a5c5cd278c49e76e8c7a4971e461967db6581
THEPADA <PaulDavidZuercher@users.noreply.github.com>
2016-06-19 04:04:35 +0200

    Update sqlConnect.php

--- sqlConnect.php
+++ sqlConnect.php
@@ -1,1 +1,6 @@
-$conn = new mysqli("localhost", "root", "rabadons0p", "PSWdatabase");
+$mysqli = new mysqli("localhost", "root", "rabadons0p", "PSWdatabase");
+
+if($mysqli->connect_errno)
+{
+  echo ("MySql Connection: " . $mysqli->connect_error)
+}
