2ada79f51612874b5ac78098e5eaeb9c05c83748
PaulDavidZuercher <padazue@googlemail.com>
2016-06-20 20:16:23 +0000

    Next steps on IndexProc

--- indexProc.php
+++ indexProc.php
@@ -14,39 +14,40 @@
     }
     
     switch($SetorGet)
-    {
-        case: 1
+	{
+		case 1:
             
-            $mysqli->prepare("INSERT INTO savedLoginData (userName, loginDataName, passWord, dataCreation) VALUES ('?', '?', '?', '?')");
-            $mysqli->bind_param("ssss",$_SESSION["userName"], $_POST["loginDataName"], $_POST["passWord"], $dataCreation);
-            break;
-        case: 2
-            
-            break;        
-            
-            $res = $mysqli->prepare("Select * where userName = ?");
-            
-            while($row = $res->fetch_row())
-            {
-                
-            }
-            
-            
-            /*
-            $mysqli->prepare("SELECT * FROM savedLoginData SLD, user U WHERE U.userName = SLD.userName AND U.userName = ?");
-		$mysqli->bind_param("s", $_POST["userName"])
-		$res = mysqli_query($mysqli); 		
-		
-		echo "<table>";
-		while($row = mysqli_fetch_object($res))
+			$stmt = $mysqli->prepare("INSERT INTO savedLoginData (userName, loginDataName, passWord, dataCreation) VALUES ('?', '?', '?', '?')");
+			$stmt->bind_param("ssss",$_SESSION["userName"], $_POST["loginDataName"], $_POST["passWord"], $dataCreation);
+			if(!$stmt->execute())
 			{
-			echo "<tr>";
-			echo "<td>",$row->,"</td>";
-			echo "<td>",$row->urlname,"</td>";
-			echo "</tr>";
-		}
-		echo "</table>";
-            */
-    }
-    
+				die("Insertion of Data failed:". $mysqli->error);
+			}
+			break;
+
+		case 2:
+			$stmt = $mysqli->prepare("Select * where userName = ?");
+         		$stmt->bind("?", $_SESSION["userName"]); 
+			$stmt->execute();
+
+			echo("</table>");
+			echo("<tr> <th>Website</th> <th> Username </th> <th> Password </th> <th> Data Created</th>"); 
+
+			while($row = $res->fetch_row())
+    			{
+				echo ("<tr>");
+				echo ("<td>" . $row["loginDataName"] . "</td>");
+				echo ("<td>" . $row["userName"] . "</td>");
+				echo ("<td>" . $row["passWord"]  . "</td>");
+				echo ("<td>" . $row["dataCreation"] . "</td>");
+				echo ("</tr>"); 
+			}
+            
+			echo("</table>"); 
+			$mysqli->prepare("SELECT * FROM savedLoginData SLD, user U WHERE U.userName = SLD.userName AND U.userName = ?");
+			$mysqli->bind_param("s", $_POST["userName"])
+			$res = $stmt->execute();
+			break;        
+	}
+
 ?>
