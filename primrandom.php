a28f01bd6fe0fdafa8a3375f0e4e8ea8d7ea2bcf
JakobDuehr <mcgillen1337@gmail.com>
2016-06-20 09:40:58 +0200

    Update primrandom.php

--- primrandom.php
+++ primrandom.php
@@ -1,17 +1,6 @@
 <?php
-//        include('Math/BigInteger.php');
 
-//     $a = new Math_BigInteger(2);
-//    $b = new Math_BigInteger(3);
-
-//    $c = $a->add($b);
-
-//    echo $c->toString(); // outputs 5 
-?>
-
-<?php
-
-$zufallszahl = rand(2,pow(2, 1024));
+$zufallszahl = rand(1000000,9000000);
 $i = $zufallszahl-1;
 $primtest = false;
 echo "erster Zufall: " . $zufallszahl . "\n";
@@ -31,7 +20,7 @@ while($primtest==false){
             $i = $i-1;
         }
         if(($zufallszahl % $i)==0 AND $i != 1){
-            $zufallszahl = rand(2,pow(2,1024));
+            $zufallszahl = rand(1000000,9000000);
             $i = $zufallszahl -1;
             //echo "Keine Primzahl";
             //$primtest=true;
