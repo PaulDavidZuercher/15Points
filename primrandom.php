fc24fd80dfee5755716245582080d6781f6224bb
JakobDuehr <mcgillen1337@gmail.com>
2016-06-20 10:03:26 +0200

    code verlängert

--- primrandom.php
+++ primrandom.php
@@ -1,18 +1,16 @@
 <?php
-
-$zufallszahl = rand(1000000,9000000);
+$zufallszahl = rand(2,1000);
 $i = $zufallszahl-1;
 $primtest = false;
-echo "erster Zufall: " . $zufallszahl . "\n";
-
+//echo "erster Zufall: " . $zufallszahl . "\n";
 
 while($primtest==false){    
     if($i == 1){
         $primtest = true;
-        echo "erzeugte Primzahl: " . $zufallszahl;
+        echo "erzeugte Primzahl p : " . $zufallszahl . "\n";
+        $p = $zufallszahl;
         break;
     }
-
     
     
     while($i > 1){
@@ -20,17 +18,46 @@ while($primtest==false){
             $i = $i-1;
         }
         if(($zufallszahl % $i)==0 AND $i != 1){
-            $zufallszahl = rand(1000000,9000000);
+            $zufallszahl = rand(2,1000);
             $i = $zufallszahl -1;
-            //echo "Keine Primzahl";
-            //$primtest=true;
             break;
             
         }
-
     }
     
 }
 
+
+
+$zufallszahl2 = rand(2,1000);
+$i2 = $zufallszahl2-1;
+$primtest2 = false;
+//echo "zweiter Zufall: " . $zufallszahl2 . "\n";
+
+while($primtest2==false){    
+    if($i2 == 1){
+        $primtest2 = true;
+        echo "erzeugte Primzahl q : " . $zufallszahl2 . "\n";
+        $q = $zufallszahl2;
+        break;
+    }
+    
+    
+    while($i2 > 1){
+        if(($zufallszahl2 % $i2) != 0 AND $i2 != 1){
+            $i2 = $i2-1;
+        }
+        if(($zufallszahl2 % $i2)==0 AND $i2 != 1){
+            $zufallszahl2 = rand(2,1000);
+            $i2 = $zufallszahl2 -1;
+            break;
+            
+        }
+    }
+    
+}
+
+$n = $p*$q;
+echo "n : " . $n;
 
 ?>
