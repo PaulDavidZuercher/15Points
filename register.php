9df63405c324e10c932c3980457774eebc22bb71
TabeaKruk <tabeavkruk@t-online.de>
2016-06-22 12:05:02 +0200

    Update register.php

--- register.php
+++ register.php
@@ -7,33 +7,19 @@
 		<script type="text/javascript">
 		function checkEingaben()
 		{
-  			if (window.XMLHttpRequest)
-  			{
-  				// AJAX nutzen mit IE7+, Chrome, Firefox, Safari, Opera
-  				xmlhttp=new XMLHttpRequest();
-			}
- 		  	else
-			{
- 		       		// AJAX mit IE6, IE5
-		        	xmlhttp=new ActiveXObject("Microsoft.XMLHTTP");
-    			}
-		    	xmlhttp.onreadystatechange=function()
-		    	{
-        		if (xmlhttp.readyState==4 && xmlhttp.status==200)
-		        {
-		        	document.getElementById("warnung").innerHTML=xmlhttp.responseText;
-        		}
-		}
-		    	xmlhttp.open("POST","registerProc.php?",true);
-		    	xmlhttp.send();		
+  			$.ajax({
+				type: "POST",
+				rul:"/registerProc.php",
+				data {userName: $("#userName").val, E-MailAdresse: $('#E-MailAdresse').val, passWord: $('#passWord').val, passWord2: $('#passWord2')},
+				success:function(result){$('#output').html(result)}
+			})	
 		}
 		
-
 		$(document).ready(
         	$("#btnUserInfo").click(
           	function(e){
             	e.preventDefault();
-            	CheckEingaben();
+            	checkEingaben();
             	return false;
 		</script>
 	</head>
@@ -44,22 +30,22 @@
 		
 			<form action="registerProc.php">
 	      		Username:<br>
-		      	<input type="text" name="uid">
+		      	<input type="text" name="userName" id="userName">
 		      	<br>
 		      	E-Mailadresse:<br>
-		      	<input type="text" name="ema">
+		      	<input type="text" name="E-MailAdresse" id="E-MailAdresse">
 		      	<br>
       			Password:<br>
-     		 	<input type="password" name="psw">
+     		 	<input type="password" name="passWord" id="passWord">
      		 	<br>
      		 	Password bestaetigen: <br>
-     		 	<input type="password" name="psw2" >
+     		 	<input type="password" name="passWord2" id="passWord2">
 			
 			
     		</form>
   		<br>
-		<button id="btnUserInfo" href="#" onclick="passwortcheck()"; return false;>Register</button>
-		<button class"buttonBlue" href="login.php">Log In </button> 
+		<a id="btnUserInfo" href="#" onclick="checkEingaben();return false;">Register</a>
+		<a class"buttonBlue" href="login.php">Log In </a> 
 		<div id="output" style="width:200px;30px">
 		</div>
 	</body>
