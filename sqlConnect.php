9c02d6bf8595ff1221b82dd955f4381d681e4da2
Im a Matrix Hax0r <oneplus.push1@gmail.com>
2016-06-22 17:22:46 +0200

    Update sqlConnect.php

--- sqlConnect.php
+++ sqlConnect.php
@@ -1,6 +1,6 @@
 $mysqli = new mysqli("localhost", "paul", "rabadons0p", "15PointsDB");
 
-if($mysqli->connect_errno)
+if($mysqli->connect_error)
 {
   echo ("MySql Connection: " . $mysqli->connect_error);
 }
