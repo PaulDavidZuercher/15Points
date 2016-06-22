34006f2200a2a47e379d7e47e31f1674c53deb61
THEPADA <PaulDavidZuercher@users.noreply.github.com>
2016-06-22 12:32:06 +0200

    Update crypto.php

--- crypto.php
+++ crypto.php
@@ -6,26 +6,26 @@ generateRandomprimeNumbers();
 function generateRandomprimeNumbers()
 {
   $randomNumber = mt_rand (0, PHP_INT_MIN);
-  $aOPP[];//Array Of Probbable primes 
+  $aOPP[] = array();//Array Of Probbable primes 
   
   for($i = 0; $i < PHP_INT_MIN; $i++);
   {
     $aOPP[] = true;
   }
   
-  for($indexOfNumber = 2; $indexOfNumber < count($aOPP))
+  for($indexOfNumber = 2; $indexOfNumber < count($aOPP); $indexOfNumber++)
   {
     if($aOPP[$indexOfNumber])
     {
        $number = $indexOfNumber +1;
        while($number < count($aOPP)){
          $aOPP[$number-1] =false;
-         $number = $number + ($indexOfNumber +1) :
+         $number = $number + ($indexOfNumber +1);
        }
     }
   }
   
-  $pimes[];
+  $pimes[] = array();
   
   for($i = 0; $i < (count ($aOPP)-1); $i++);
   {
