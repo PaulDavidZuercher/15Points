422422c9f43ad17fa7b14721a284e6c953e19203
marielulu <lisa-marie.goebel@web.de>
2016-06-24 00:43:42 +0200

    Update decrypt.php

--- decrypt.php
+++ decrypt.php
@@ -1,21 +1,53 @@
 <?php
-$decryptedTxt = (pow ($txt ,$e )) % $n;
-array(
+
+$durchzaehlen = 0;
+$decryptTxt = array;
+
+while ($durchzaehlen >= $txt.length) { //für jedes einzele Zeichen des übergebenen txt´s ausführen
+  $zwischenZeichen = substr ($txt, $durchzaehlen, 1); //liest einzelnes Zeichen aus dem übergebenen txt-String aus
+  $zeichen($zwischenZeichen); //nimmt das aus dem txt-string ausgewählte Zeichen und sucht es im array "zeichen"
+  $decryptedZeichen = (pow ($zeichen ,$e )) % $n; // von mathematik.de
+  $durchzaehlen++;
+  $decryptTxt["$durchzaehlen"] = $decryptedZeichen;
+}
+  }
+  
+$zeichen = array( //falsch herum (aber richtig für encrxpt)
+  01 => "1",
+  02 => "2",
+  03 => "3",
+  04 => "4",
+  05 => "5",
+  06 => "6",
+  07 => "7",
+  08 => "8",
+  09 => "9",
+  
   1  => "A",
   2 => "B",
   3 => "C",
   4 => "D",
   5 => "E",
-  6 => C,
-  7 => C,
-  8 => C,
-  9 => C,
-  10 => C,
-  11 => C,
-  12 => C,
-  13 => C,
-  3 => C,
-  3 => C,
-  3 => C,
+  6 => "F",
+  7 => "G",
+  8 => "H",
+  9 => "I",
+  10 => "J",
+  11 => "K",
+  12 => "L",
+  13 => "M",
+  14 => "N",
+  15 => "O",
+  16 => "P",
+  17 => "Q",
+  18 => "R",
+  19 => "S",
+  20 => "T",
+  21 => "U",
+  22 => "V",
+  23 => "W",
+  24 => "X",
+  25 => "Y",
+  26 => "Z",
 )
 ?>
