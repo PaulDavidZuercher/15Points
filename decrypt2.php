ba4f076bacbebf8288557146535d45cee934ba9e
marielulu <lisa-marie.goebel@web.de>
2016-06-29 10:11:15 +0200

    Update decrypt2.php

--- decrypt2.php
+++ decrypt2.php
@@ -1,63 +1,67 @@
 <?php
-decrypt("119", 21, 4);
-function decrypt ($txt, $prK, $n)
+
+decrypt("23", 12, 3);
+function decrypt ( $txt, $puK, $n )
 {
-    $d = $prK;
+  $e = $puK;
   $durchzaehlen = 0;
   $decryptTxt = array ();
   $txtLaenge = strlen ($txt);
   
   $zeichen = array
   ( 
-     "01" => "A",
-    "02" => "B",
-    "03" => "C",
-    "04" => "D",
-    "05" => "E",
-    "06" => "F",
-    "07" => "G",
-    "08" => "H",
-    "09" => "I",
-    "10" => "J",
-    "11" => "K",
-    "12" => "L",
-    "13" => "M",
-    "14" => "N",
-    "15" => "O",
-    "16" => "P",
-    "17" => "Q",
-    "18" => "R",
-    "19" => "S",
-    "20" => "T",
-    "21" => "U",
-    "22" => "V",
-    "23" => "W",
-    "24" => "X",
-    "25" => "Y",
-    "26" => "Z",
+    "A"  => "01",
+    "B" => "02",
+    "C" => "03",
+    "D" => "04",
+    "E" => "05",
+    "F" => "06",
+    "G" => "07",
+    "H" => "08",
+    "I" => "09",
+    "J" => "10",
+    "K" => "11",
+    "L" => "12",
+    "M" => "13",
+    "N" => "14",
+    "O" => "15",
+    "P" => "16",
+    "Q" => "17",
+    "R" => "18",
+    "S" => "19",
+    "T" => "20",
+    "U" => "21",
+    "V" => "22",
+    "W" => "23",
+    "X" => "24",
+    "Y" => "25",
+    "Z" => "26",
   
-    "27" => "1",
-    "28" => "2",
-    "29" => "3",
-    "30" => "4",
-    "31" => "5",
-    "32" => "6",
-    "33" => "7",
-    "34" => "8",
-    "35" => "9",
+    "1" => "27",
+    "2" => "28",
+    "3" => "29",
+    "4" => "30",
+    "5" => "31",
+    "6" => "32",
+    "7" => "33",
+    "8" => "34",
+    "9" => "35",
   );
   
  
-  while ($durchzaehlen <= $txtLaenge) //für jedes einzele Zeichen des übergebenen txt´s ausführen 
+ $zwischenArray = explode(" ", $txt);
+ $arrayLaenge = count($zwischenArray);
+  while ($durchzaehlen <= $arrayLaenge)
   { 
-    $zwischenZeichen = substr ($txt, $durchzaehlen, 1); //liest einzelnes Zeichen aus dem übergebenen txt-String aus
-    $zeichenImArray = $zeichen[$zwischenZeichen]; //nimmt das aus dem txt-string ausgewählte Zeichen und sucht es im array "zeichen"
-    $decryptedZeichen = (pow ($zeichenImArray ,$d )) % $n; // von mathematik.de
-    $decryptTxt["$durchzaehlen"] = $decryptedZeichen; //fügt zu Gesamt_decryptedTxt_array die einzelnen encrypted Zeichen hinzu
+    $zwischenZeichen = $zwischenArray[$durchzaehlen]; 
+    $zwischenZeichen2 = (pow ($zwischenZeichen ,$e )) % $n; // von mathematik.de
+    $decryptedZeichen = $zeichen[$zwischenZeichen2]; //nimmt das aus dem txt-string ausgewählte Zeichen und sucht es im array "zeichen"
+    $decryptTxt["$durchzaehlen"] = $decryptedZeichen; //fügt zu Gesamt_decryptedTxt_array die einzelnen decrypted Zeichen hinzu
    $durchzaehlen++;
   }
 $decryptedTxtString = implode("",$decryptTxt) ;
-echo "Verschlüsselter Text:" . $decryptedTxtString;
-return $decryptedTxtString; //Rückgabe des entschlüsselten $txt-Passworts
+echo "Entschlüsselter Text:" . $decryptedTxtString;
+return $decryptedTxtString; //Rückgabe des verschlüsselten $txt-Passworts
+  
 }
 ?>
