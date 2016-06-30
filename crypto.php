01ce42145823bbd1224df9e819a07dd8f1603d8c
PaulDavidZuercher <Padazue@googlemail.com>
2016-06-30 11:43:21 +0000

    nicer Prime showing

--- crypto.php
+++ crypto.php
@@ -7,7 +7,7 @@ echo nl2br("finished \n");
 
 function generateRandomprimeNumbers()
 {
-  $maxGenInt = 21474836;
+  $maxGenInt = 2147;
   $randomNumber = mt_rand (0, $maxGenInt);
   $aOPP = [];//Array Of Probbable primes 
   for($i = 0; $i < $randomNumber; $i++)
