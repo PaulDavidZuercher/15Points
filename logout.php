063f99a3e8333f88fede33494ab136842886eb2a
PaulDavidZuercher <Padazue@googlemail.com>
2016-06-30 22:44:34 +0000

    added logout page

--- logout.php
+++ logout.php
@@ -3,8 +3,15 @@ session_start();
 if ($_SESSION["login"])
 {
 	if (!session_destroy())
-		die ("Logour failed")
+		die ("Logour failed");
 	
 	$_SESSION["login"] = false;
 	$_SESSION["userName"] = "";	
+	
+	echo ("<html> <head> <script> window.location=\"login.php\"</script></head> <body><p> redirecting... </body></html>");	
 }
+
+else{
+	echo ("You are not logged in :)");
+}
+?>
