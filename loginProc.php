941714da01311d6b071057984562059bc3febae4
PaulDavidZuercher <Padazue@googlemail.com>
2016-06-30 19:37:55 +0000

    fixing

--- loginProc.php
+++ loginProc.php
@@ -5,30 +5,22 @@
   //checkt username mit pw in mysql datenbank existieren --> öffenet index oder gibt fehlercode aus
   if(!empty($_POST))
   {
-    $userName = $_POST["userName"];
-    $passWord = $_POST["passWord"];
-    //if($res = $mysqli->query("Select * from user where userName = '$userName' && passWord = '$passWord'"))
-    //{
-    // 	echo ("Selected Rows: " . $res->num_rows );
-    //}
-    if($stmt =$mysqli->prepare("Select * from user where userName = '?' && passWord = '?'")){
-      $res = $stmt->get_result();
-      echo("test: " . $res);
-      if($res->num_rows > 0)
-        {
-        //user exists 
-         $_SESSION["login"] = true;
-         $_SESSION["userName"] = $_POST["userName"]; //todo find more safe version :)
-        
-         echo("<script type=\"text/javascript\"> window.location = \"/index.php\"; </script>" );
-         exit();
+    $userName = $mysqli->real_escape_string($_POST["userName"]);
+    $passWord = $mysqli->real_escape_string($_POST["passWord"]);
+    
+    if($res = $mysqli->query("Select * from user where userName = '$userName' && passWord = '$passWord'"))
+    {
+      if($res->num_rows > 0){
+        die("<script type=\"text/javascript\"> window.location = \"/index.php\"; </script>" );
+        exit();
       }
       else{
-        die ("failed to prepare statement");
-
+        die("<p> invalid Username/Password Combination </p>");
       }
-    
-      echo("<p> invalid Username/Password Combination </p>");
     }
+    else{
+       die("failed to query");
+    }
+    
   }
 ?>
