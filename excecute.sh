08071f9f92f7cd17b810481a950291d2833470a5
PaulDavidZuercher <Padazue@googlemail.com>
2016-06-30 22:39:44 +0000

    added logout page

---
+++ excecute.sh
@@ -0,0 +1,3 @@
+rm -rf /var/www/html/*:w
+
+cp -r . /var/www/html/
--- logout.php
+++ logout.php
@@ -1,1 +1,10 @@
-
+<?php
+session_start();
+if ($_SESSION["login"])
+{
+	if (!session_destroy())
+		die ("Logour failed")
+	
+	$_SESSION["login"] = false;
+	$_SESSION["userName"] = "";	
+}
