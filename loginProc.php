eef2fb37c407bf64b3143ab380fe2ca2d4c415cb
PaulDavidZuercher <Padazue@googlemail.com>
2016-06-30 18:09:59 +0000

    fixing

--- loginProc.php
+++ loginProc.php
@@ -6,27 +6,21 @@
   if(!empty($_POST))
   {
     $userName = $_POST["userName"];
-    if( $stmt =$mysqli->prepare("Select * from user where userName = ? && passWord = ?")){
-    $stmt->bind_param("ss", $_POST["userName"], $_POST["passWord"] );
-    $stmt->execute();
-    $res = $stmt->get_result();
+    if($stmt =$mysqli->prepare("Select * from user where userName = ? && passWord = ?")){
+      $stmt->bind_param("ss", $_POST["userName"], $_POST["passWord"] );
+      $stmt->execute();
+      $res = $stmt->get_result();
       if($res->num_rows > 0)
       {
         //user exists 
-        $_SESSION["login"] = true;
-        $_SESSION["userName"] = $_POST["userName"]; //todo find more safe version :)
+         $_SESSION["login"] = true;
+         $_SESSION["userName"] = $_POST["userName"]; //todo find more safe version :)
         
-        echo("<script type=\"text/javascript\"> window.location = \"/index.php\"; </script>" );
-        exit();
-        
+         echo("<script type=\"text/javascript\"> window.location = \"/index.php\"; </script>" );
+         exit();
       }
-      else{
-          echo("Query Error" . $stmt->error);
-      }
-      
-      echo("<p> invalid Username/Password Combination </p>");
-      
-    }
     
+      echo("<p> invalid Username/Password Combination </p>");
+    }
   }
 ?>
