60e4df832f8e81b0b22d68a17493e77da81db32d
PaulDavidZuercher <Padazue@googlemail.com>
2016-06-30 19:07:21 +0000

    Validated sqlConnect

--- sqlConnect.php
+++ sqlConnect.php
@@ -1,7 +1,8 @@
 <?php $mysqli = new mysqli("localhost", "paul", "rabadons0p", "15PointsDB");
 
-if($mysqli->connect_error)
+if($mysqli->connect_errno)
 {
-  echo ("MySql Connection: " . $mysqli->connect_error);
+  die("MySql Connection: " . $mysqli->connect_error);
+
 }
 ?>
