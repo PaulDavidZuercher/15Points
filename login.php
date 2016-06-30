97fed247212cbff930d8090a3466062245331484
Im a Matrix Hax0r <oneplus.push1@gmail.com>
2016-06-30 23:39:35 +0200

    Update login.php

--- login.php
+++ login.php
@@ -73,8 +73,8 @@ $(document).ready(function(){
       		</div>
       		
       		<div class="buttonBox">
-      		<a id="btnUserInfo" class="buttonBlueLeft">Login</a> 
-        	<a class="buttonBlueRight" href="register.php">Register</a>
+      		<a id="btnUserInfo" class="buttonHome">Login</a> 
+        	<a class="buttonHome" href="register.php">Register</a>
         	</div>
         	
       	</form>
