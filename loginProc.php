dcdd74f204f20c83a915a20072625d93a1211625
PaulDavidZuercher <Padazue@googlemail.com>
2016-06-30 20:57:31 +0000

    adding Debug inf

--- loginProc.php
+++ loginProc.php
@@ -5,8 +5,8 @@
   //checkt username mit pw in mysql datenbank existieren --> öffenet index oder gibt fehlercode aus
   if(!empty($_POST))
   {
-    $userName = $mysqli->real_escape_string($_POST["userName"]);
-    $passWord = $mysqli->real_escape_string($_POST["passWord"]);
+    $userName = $_POST["userName"];
+    $passWord = $_POST["passWord"];
     echo ("Username: '$userName', PassWord '$passWord'"); 
     if($res = $mysqli->query("Select * from user where userName = '$userName' && passWord = '$passWord'"))
     {
