7222207cace12eef16d5d02db0d128c8fd7830dc
PaulDavidZuercher <Padazue@googlemail.com>
2016-06-30 22:21:42 +0000

    Dynamic loading ...

--- index.php
+++ index.php
@@ -12,11 +12,11 @@
 	
 		function addLoginDataToServer() //übernehmen der Daten aus dem Formular per POST Methode; sofortiges tabellarisches Wiederanzeigen 
 		{
-			var name = $('Name').val();
-			var userName = $('userName').val();
-			var passWord = $('passWord').val();
+			var name = $('#Name').val();
+			var userName = $('#userName').val();
+			var passWord = $('#passWord').val();
 
-			$.post("indexProc.php",{'SetOrGet': 1,'name' : name, 'userName' : userName, 'passWord' : passWord, function (data, status){alert("result :" +data)});
+			$.post("indexProc.php",{'SetOrGet': 1,'loginDataName' : name, 'userName' : userName, 'passWord' : passWord, function (data, status){alert("result :" +data)});
 			
 		}
 		function addLoginDataToTable()
@@ -24,9 +24,19 @@
 			$.post(
 				"indexProc.php",
 				{'SetOrGet': 2},
-				function(result, status){$("#passwordView").html = result}
+				function(result, status){$("#passwordView").html(result);}
 			);
 		}
+		
+		$(document).ready(function (){
+			$(".buttonDiv").click(
+			function(e){
+			  e.preventDefault();
+			  addLoginDataToServer();
+			  addLoginDataToTable();
+			  return false;
+			});
+			});
 	</script>
 </head>
 
@@ -45,13 +55,13 @@
 		<form>
 			<div class="AddDiv">
 				<div class="miniDiv">
-					<input class="logInput" type="text" name="userName" placeholder="Name" id="Name">
+					<input id="logInput" type="text" name="userName" placeholder="Name" id="Name">
 				</div>
 				<div class="miniDiv">
-					<input class="logInput" type="text" name="userName" placeholder="Username" id="userName">
+					<input id="logInput" type="text" name="userName" placeholder="Username" id="userName">
 				</div>
 				<div class="miniDiv">
-					<input class="logInput" type="password" name="passWord" placeholder="Password" id="passWord">
+					<input id="logInput" type="password" name="passWord" placeholder="Password" id="passWord">
 				</div>	
 			</div>
 			
