d86a331ae399acd27b1ec8d1263317887285b50a
PaulDavidZuercher <Padazue@googlemail.com>
2016-06-30 11:34:51 +0000

    nicer Prime showing

--- crypto.php
+++ crypto.php
@@ -7,16 +7,15 @@ echo nl2br("finished \n");
 
 function generateRandomprimeNumbers()
 {
-  $maxGenInt = 200;
+  $maxGenInt = 2147483600;
   $randomNumber = mt_rand (0, $maxGenInt);
   $aOPP = [];//Array Of Probbable primes 
   for($i = 0; $i < $randomNumber; $i++)
   {
-	echo nl2br("working: maxGenInt Progress:  $i from $randomNumber \n");
 	$aOPP[] = true;
+	ob_end_clean();
   }
-  echo nl2br("Start on removing:");
-  
+   
   for($indexOfNumber = 1; $indexOfNumber < $randomNumber;$indexOfNumber++)
   {
     if($aOPP[$indexOfNumber])
@@ -25,8 +24,6 @@ function generateRandomprimeNumbers()
 	while( $buffNumInd < count($aOPP))
         {
 		$buffNumInd = $buffNumInd + ($indexOfNumber +1);
-		echo nl2br("removing: " .($buffNumInd + 1). "\n");
-		
 		$aOPP[$buffNumInd] =false;
 	}
     }
