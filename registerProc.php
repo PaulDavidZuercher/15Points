3bd114097413b3773e438fcb51c4ae7d315a626f
PaulDavidZuercher <Padazue@googlemail.com>
2016-07-01 14:29:46 +0000

    Changed few things

--- registerProc.php
+++ registerProc.php
@@ -1,21 +1,17 @@
 <?php 
-// todo: prevent sql injection
-
 include 'sqlConnect.php';
+
 $userName = $_POST["userName"];
 $E_MailAdresse = $_POST["E_MailAdresse"];
 $passWord = $_POST["passWord"];
-$accountCreation = new DateTime('now', new DateTimeZone('Europe/London'));
-
 $ergebnis = $mysqli->query("SELECT userName FROM user WHERE userName = '". $mysqli->real_escape_string('$userName')."' ");
 
 if($ergebnis->fetch_row())
 {
-	echo "Username existiert bereits"; 
+	echo ("Username existiert bereits"); 
 	exit();
 }
 
-$mysqli->query("INSERT INTO user (userName, passWord, email, reg_date) VALUES ('$userName', '$passWord', '$E_MailAdresse', '$accountCreation')");
-
- echo "Regestrierung abgeschloßen. Willkommen ('$userName').";
+$mysqli->query("INSERT INTO user (userName, passWord, email) VALUES ('$userName', '$passWord', '$E_MailAdresse'");
+echo ("Regestrierung abgeschloßen. Willkommen ('$userName').");
 ?>
