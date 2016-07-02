894c19e2c3de74c39c11efd91d6369420500e35f
PaulDavidZuercher <Padazue@googlemail.com>
2016-07-02 20:38:19 +0000

    fixed setupDatabase

--- setupDatabase.php
+++ setupDatabase.php
@@ -4,7 +4,7 @@ include 'sqlConnect.php'
 if(!$mysqli->query("DROP TABLE IF EXISTS users"))
 	die("Error" . $mysqli->error);
 
-if!($mysqli->query("Create Table savedLoginData (userName varchar(64) NOT NULL, foreignServiceName varchar(64), foreignServiceUserName varchar(64) NOT NULL, foreignServicePassWord varchar(64) NOT NULL, creationTimeStamp TIMESTAMP DEFAULT CURRENT_TIMESTAMP);")
+if!($mysqli->query("Create Table savedLoginData (userName varchar(64) NOT NULL, foreignServiceName varchar(64), foreignServiceUserName varchar(64) NOT NULL, foreignServicePassWord varchar(64) NOT NULL, creationTimeStamp TIMESTAMP DEFAULT CURRENT_TIMESTAMP);"))
 	die("Error" . $mysqli->error);
 ?>
 
