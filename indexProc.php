1271ea2749c9478649c529bc2f24f6916e4f13c8
PaulDavidZuercher <Padazue@googlemail.com>
2016-07-03 22:03:37 +0000

    fixed indexProc

--- indexProc.php
+++ indexProc.php
@@ -9,29 +9,24 @@
     
     $cre_date = new DateTime('now', new DateTimeZone('Europe/London'));
     $SetorGet = $_POST["SetOrGet"];
-    if($_POST["userName"] != $_SESSION["userName"])
-    {
-        die("Try Not to Hack me m8 :) you are not {$_POST["userName"]} :)");
-    }
     
     switch($SetorGet) // Two way use: 1 as setting data 2 as getting data
 	{
 		case 1:
-           		$userName = $mysqli->real_escape_string($_POST["userName"]);
+           		$userName = $mysqli->real_escape_string($_SESSION["userName"]);
 			$foreignServiceName = $mysqli->real_escape_string($_POST["foreignServiceName"]);
 			$foreignServiceUserName = $mysqli->real_escape_string($_POST["foreignServiceUserName"]);
 			$foreignServicePassWord = $mysqli->real_escape_string($_POST["foreignServicePassWord"]);
-			$stmt = $mysqli->query("INSERT INTO savedLoginData (userName, foreignServiceName, foreignServiceUserName, foreignServicePassWord) VALUES ('$userName', '$foreignServiceName', '$foreignServiceUserName', '$foreignServicePassWord');");
-			if(!$stmt->execute())
-			{
+			$res = $mysqli->query("INSERT INTO savedLoginData (userName, foreignServiceName, foreignServiceUserName, foreignServicePassWord) VALUES ('$userName', '$foreignServiceName', '$foreignServiceUserName', '$foreignServicePassWord');");
+			if(!$res)
 				die("Insertion of Data failed:". $mysqli->error);
-			}
+			
 			break;
 
 		case 2:
-			$res = $mysqli->query("SELECT * FROM savedLoginData WHERE userName = {$_POST["userName"]} ");	
+			$res = $mysqli->query("SELECT * FROM savedLoginData WHERE userName = '{$_POST["userName"]}'");	
 
-			echo("</table>");
+			echo("<table>");
 			echo("<tr> <th>Website</th> <th> Username </th> <th> Password </th> <th> Data Created</th> </tr>"); 
 
 			while($row = $res->mysqli_fetch_array())
