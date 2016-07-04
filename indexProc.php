bba6c99a464df3a5794b6e0ac7916e6700a86d52
THEPADA <PaulDavidZuercher@users.noreply.github.com>
2016-07-04 13:14:24 +0200

    Added alert for password older than a year

--- indexProc.php
+++ indexProc.php
@@ -37,8 +37,10 @@
 				echo ("<td>" . $row["foreignServiceUserName"] . "</td>");
 				echo ("<td>" . $row["foreignServicePassWord"]  . "</td>");
 				
-				$daysdiff = abs(strtotime($row["creationTimeStamp"]) - strtotime(date("Y-m-d H:i:s")));
-				echo ("<td>" . $daysdiff. "</td>");
+				$daysdiff = floor(abs(strtotime($row["creationTimeStamp"]) - strtotime(date("Y-m-d H:i:s")))/60/60/24);
+				if($daysdiff > 365)
+					echo("<script>alert(\"Das Passwort für {$row["foreignServiceName"]} ist älter als ein jahr); \")</script>")
+				echo ("<td>" . $daysdiff. "Tage alt" . "</td>");
 				echo ("</tr>"); 
 			}
             
